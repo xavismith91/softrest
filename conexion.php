09b7bee7af2eaac9ca21d24f322bf0255da5a649
unknown <xaviersolano91@gmail.com>
2023-01-02 08:49:36 -0600

    ordylog

---
+++ conexion.php
@@ -0,0 +1,12 @@
+<?php
+
+$con=mysqli_connect("mysql.webcindario.com","dsiinventario","rkGLKUKBSmMbnh2e","dsiinventario");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+
+?>
